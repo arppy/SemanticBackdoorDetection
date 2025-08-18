5f2b1b2d68684a864cf8f71f4d839d4c6d98fe64
arppy <berta@inf.u-szeged.hu>
2025-08-18 11:55:28 +0200

    script for train multiple standard models

--- multi_model_train.sh
+++ multi_model_train.sh
@@ -14,11 +14,7 @@ for i in $(seq $from $1); do
   done
   seed=${randseed:0:9}
   dataseed=${randdataseed:0:9}
-  #python model_train.py --seed $seed --data_seed $dataseed --batch 1024 --gpu 0 --epochs 400 --adversarial --ddpm_path ../res/data/cifar10_ddpm.npz > "nohup_cifar10_s"$seed"_b1024_e400_ro.out" 2>&1
-  python model_train.py --seed $seed --data_seed $dataseed --batch 1024 --epochs 400 --adversarial --backdoor_dataset torchvision.datasets.CIFAR100 --backdoor_class $bd_t --ddpm_path ../res/data/cifar10_ddpm.npz --ddpm_backdoor_path ../res/data/cifar100_ddpm.npz --gpu 3 > "nohup_cifar10_backdoor_resnet18_s"$seed"_b1024_e400_ro.out" 2>&1
-  #python model_train.py --seed $seed --data_seed $dataseed --batch 1024 --epochs 400 --adversarial --ddpm_path ../res/data/cifar10_ddpm.npz --gpu 3 > "nohup_cifar10_resnet18_s"$seed"_b1024_e400_ro.out" 2>&1
-  #python model_train.py --seed $seed --data_seed $dataseed --dataset torchvision.datasets.CIFAR10 --batch 128 --epochs 100 --gpu 0 --model_architecture preact18 > "standard_preact18_train_"$seed".out" 2>&1
-  #python model_train.py --seed $seed --data_seed $dataseed --dataset torchvision.datasets.CIFAR10 --batch 100 --epochs 100 --gpu 0 > "standard_resnet18_train_"$seed".out" 2>&1
-  #python model_train.py --seed $seed --data_seed $dataseed --dataset torchvision.datasets.CIFAR10 --batch 128 --epochs 100 --gpu 1 --model_architecture preact18 --backdoor_dataset torchvision.datasets.CIFAR100 --backdoor_class $bd --target_class $target > "standard_cifar10_preact18_train_"$seed"_"$target"-"$bd".out" 2>&1
+  #python model_train.py --seed $seed --data_seed $dataseed --batch 1024 --epochs 400 --adversarial --backdoor_dataset torchvision.datasets.CIFAR100 --backdoor_class $bd_t --ddpm_path ../res/data/cifar10_ddpm.npz --ddpm_backdoor_path ../res/data/cifar100_ddpm.npz --gpu 3 > "nohup_cifar10_backdoor_resnet18_s"$seed"_b1024_e400_ro.out" 2>&1
+  python model_train.py --seed $seed --data_seed $dataseed --batch 100 --epochs 100 --backdoor_dataset torchvision.datasets.CIFAR100 --backdoor_class $bd_t --gpu 1 > "nohup_cifar10_backdoor_resnet18_s"$seed"_b100_e100_st.out" 2>&1
 done
 
